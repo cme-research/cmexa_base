31116d3b3f6bfc8fed0466da9fbf9e1f92cabd55
cme <info@cme-robotics.com>
2025-09-03 11:02:09 +0200

    Changed parameter to hardware interface.

--- src/mecanumbot_system.cpp
+++ src/mecanumbot_system.cpp
@@ -81,7 +81,8 @@ hardware_interface::CallbackReturn CmexaMecanumBotSystemHardware::on_init(
     hardware_interface::stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
   // END: This part here is for exemplary purposes - Please do not copy to your production code
 
-  wheel_radius_ = std::stod(info_.hardware_parameters["wheel_radius"]);
+  wheel_radius_ =
+    hardware_interface::stod(info_.hardware_parameters["wheel_radius"]);
 
 
   for (const hardware_interface::ComponentInfo & joint : info_.joints)
